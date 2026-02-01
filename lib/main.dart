46426d962ac9423d1987dedb1a97e0069b4e22fe
Arom2007 <arompradhan@gmail.com>
2026-02-01 14:32:43 +0545

    Tested previous concepts by making a box with 2 boxes inside each having 2 rows of Icon + Text

--- lib/main.dart
+++ lib/main.dart
@@ -13,139 +13,96 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("A R O M"),
+          title: Text("A R O M", style: TextStyle(color: Colors.white),),
           centerTitle: true,
+          backgroundColor: Colors.deepPurple,
         ),
         body: Center(
           child: Container(
-            color: Colors.black,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
+            width: 500,
+            height: 400,
+            color: Colors.purple,
+            child: Center(
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Container(
-                    color: Colors.red,
-                    height: 400,
-                    padding: EdgeInsets.all(12),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Container(
-                            height: 100,
-                            color: Colors.green,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Row(
-                                  children: [
-                                    Icon(Icons.add),
-                                    Text("A Ro M")
-                                  ],
-                                ),
-                                Row(
-                                  children: [
-                                    Icon(Icons.add_a_photo),
-                                    Text("AR OM")
-                                  ],
-                                )
-                              ],
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Container(
-                            color: Colors.blue,
-                            height: 100,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Row(
-                                  children: [
-                                    Icon(Icons.add_alarm_sharp),
-                                    Text("B Ro M")
-                                  ],
-                                ),
-                                Row(
-                                  children: [
-                                    Icon(Icons.add_a_photo_outlined),
-                                    Text("BR OM")
-                                  ],
-                                )
-                              ],
-                            ),
-                          ),
-                        )
-                      ],
-                    )
-                  ),
-                  SizedBox(width: 12,),
-                  Container(
-                      color: Colors.red,
-                      height: 400,
-                      padding: EdgeInsets.all(12),
-                      child: Row(
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Container(
+                      width: 200,
+                      height: 200,
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black,
+                            offset: Offset(5, 5),
+                            blurRadius: 8
+                          )
+                        ]
+                      ),
+                      child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Container(
-                              height: 100,
-                              color: Colors.green,
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Row(
-                                    children: [
-                                      Icon(Icons.add),
-                                      Text("A Ro M")
-                                    ],
-                                  ),
-                                  Row(
-                                    children: [
-                                      Icon(Icons.add_a_photo),
-                                      Text("AR OM")
-                                    ],
-                                  )
-                                ],
-                              ),
-                            ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.train, size: 30, color: Colors.white,),
+                              Text("Arom", style: TextStyle(fontSize: 30, color: Colors.white),)
+                            ],
                           ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Container(
-                              color: Colors.blue,
-                              height: 100,
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Row(
-                                    children: [
-                                      Icon(Icons.add_alarm_sharp),
-                                      Text("B Ro M")
-                                    ],
-                                  ),
-                                  Row(
-                                    children: [
-                                      Icon(Icons.add_a_photo_outlined),
-                                      Text("BR OM")
-                                    ],
-                                  )
-                                ],
-                              ),
-                            ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.airplanemode_active, size: 30, color: Colors.white,),
+                              Text("Barom", style: TextStyle(fontSize: 30, color: Colors.white),)
+                            ],
                           )
                         ],
-                      )
+                      ),
+                    ),
                   ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Container(
+                      width: 200,
+                      height: 200,
+                      decoration: BoxDecoration(
+                          color: Colors.blue,
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.black,
+                                offset: Offset(-5, 5),
+                                blurRadius: 8
+                            )
+                          ]
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.directions_car_filled_rounded, size: 30, color: Colors.white),
+                              Text("Carom", style: TextStyle(fontSize: 30, color: Colors.white),)
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.bike_scooter, size: 30, color: Colors.white,),
+                              Text("Darom", style: TextStyle(fontSize: 30, color: Colors.white),)
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  )
                 ],
               ),
             ),
           ),
-        ),
+        )
       )
     ); // MaterialApp
   }
