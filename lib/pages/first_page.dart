6cb7ba490abd79a000b6af7ff30a6f8a4f2d2e74
Arom2007 <arompradhan@gmail.com>
2026-02-02 15:09:53 +0545

    Fixed issue of back button of home and setting page opening drawer instead of going back

--- lib/pages/first_page.dart
+++ lib/pages/first_page.dart
@@ -21,6 +21,7 @@ class FirstPage extends StatelessWidget {
               leading: Icon(Icons.home),
               title: Text("H O M E"),
               onTap: () {
+                Navigator.pop(context);
                 Navigator.pushNamed(context, '/homepage');
               },
             ),
@@ -28,7 +29,9 @@ class FirstPage extends StatelessWidget {
               leading: Icon(Icons.settings),
               title: Text("S E T T I N G S"),
               onTap: () {
+                Navigator.pop(context);
                 Navigator.pushNamed(context, '/settingspage');
+
               },
             )
           ],
