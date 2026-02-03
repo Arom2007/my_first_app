b2771e72b253adbdb917feb0b32994f09365f281
Arom2007 <arompradhan@gmail.com>
2026-02-03 14:07:30 +0545

    Made functioning tap counter page

--- lib/pages/tap_counter_page.dart
+++ lib/pages/tap_counter_page.dart
@@ -11,18 +11,35 @@ class _TapCounterPageState extends State<TapCounterPage> {
 
   // Variable
   int _counter = 0;
-
   // Method
   void _incrementCounter() {
-    _counter++; // Any time we change the value of something and we want the change to be reflected in the app we need to use setState which requires the stateful widget
-
+    // Any time we change the value of something and we want the change to be reflected in the app we need to use setState which requires the stateful widget
+    // setState basically rebuilds the widgets
+    // Stateless widgets are widgets that don't change whatsoever
+    // Stateful widgets are widgets that change depending on the state
+    setState(() {
+      _counter++;
+    });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("T A P  C O U N T E R"),
-      ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Message
+            Text("You pushed the button this many times"),
+
+            // Counter value
+            Text(_counter.toString(), style: TextStyle(fontSize: 40),),
+
+            // Button
+            ElevatedButton(onPressed: _incrementCounter, child: Icon(Icons.add, size: 30, color: Colors.deepPurple[600],))
+          ],
+        ),
+      )
 
     );
   }
