4aedf203122fc4286e4d888ae86bfc8499713d0d
Arom2007 <arompradhan@gmail.com>
2026-02-04 11:09:56 +0545

    Made use of text field and text editing controller to take user's name as input and display a greeting message on screen using user's name.

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,13 +1,58 @@
 import 'package:flutter/material.dart';
 
-class ProfilePage extends StatelessWidget {
+
+// Using a text editing controller to get access to what the user typed
+TextEditingController myController = TextEditingController();
+
+
+// Displayed greeting message
+String greetingMessage = "";
+
+class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+
+  // Method to greet user on button press
+  void greetUser() {
+    String userName = myController.text;
+    setState(() {
+      greetingMessage = "Hello ${userName}";
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: TextField()
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
+            children: [
+              TextField(
+                controller:  myController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "Type your name" // Adds low opacity text
+
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10.0),
+                child: ElevatedButton(onPressed: greetUser, child: Icon(Icons.check)),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20.0),
+                child: Text(greetingMessage, style: TextStyle(fontSize: 20),),
+              ),
+            ],
+          ),
+        ),
+
       ),
 
     );
